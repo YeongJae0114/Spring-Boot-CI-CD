936c285998d349ada224cec91c4127d870fbd252
YeongJae0114 <yjl8628@naver.com>
2024-07-16 14:41:24 +0900

    2일차

---
+++ demo2/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MainController {
+    @GetMapping("/sbb")
+    public String index(){
+        return "안녕";
+    }
+
+    @GetMapping("/a")
+    public String hello(
+            String age, String id){
+        return "age : %s id : %s".formatted(age, id);
+    }
+
+
+}
