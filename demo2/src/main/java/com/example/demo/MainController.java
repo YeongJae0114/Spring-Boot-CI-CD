35377cc86f59873a6bcc7aa0eeb9d7bb2b0d50af
YeongJae0114 <yjl8628@naver.com>
2024-07-17 13:48:51 +0900

    3day

--- demo2/src/main/java/com/example/demo/MainController.java
+++ demo2/src/main/java/com/example/demo/MainController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,10 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    @Value("${custom.site.name}")
+    private String siteName;
+
+    @GetMapping("/")
+    public String showMain(){
+        return "Hello, World!" + siteName;
+    }
+
+
     @GetMapping("/sbb")
     public String index(){
         return "안녕";
     }
+
+
+
 
     @GetMapping("/a")
     public String hello(
